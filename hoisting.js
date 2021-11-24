760a781b25efab72193f87dddddd8ce406ecf351
Esteban Rojas Ojeda <65691243+estebanRojasOjeda@users.noreply.github.com>
2021-11-24 17:53:49 -0300

    hoisting
    add hoisting examples

---
+++ hoisting.js
@@ -0,0 +1,10 @@
+//var example
+console.log(example);
+var example = "I'm developer learing JavaScript Bro!";
+//R: undefined
+
+//let example
+console.log(example);
+let example = "I'm developer learing JavaScript Bro!";
+//R: ReferenceError: Cannot access 'example' before initialization
+//  at Object.<anonymous> (C:\Users\erojas\Documents\esteban\hoisting.js:7:13)
